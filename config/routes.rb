ff942f101b227beac61d8a9ff57a1d9786ef7f48
jordan-creyelman <creyelmanjo@gmail.com>
2021-11-02 08:31:55 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  
-  
-  # resources :users
-    # resources  :gossips
   get 'gossips/:id',to:'gossips#show',as: "gossips"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'index#home'
+  
   get '/user/:id', to: 'user#show',as: "user"
   
   get '/team', to: 'index#team'
