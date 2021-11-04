638d36b0c0709b2992e08f354599a7fc1e02de58
jordan-creyelman <creyelmanjo@gmail.com>
2021-11-04 22:23:41 +0100

    test

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -6,6 +6,6 @@ class Gossip < ApplicationRecord
   delegate :first_name, to: :user, allow_nil: true, prefix: true
   has_many :taggers
   has_many :tags, through: :taggers
-  has_many:likes
+
 end
 #user,optional: true
